e52bb451ad05e7dc50696c5a9b8614747b67e3da
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-08-20 00:44:14 +0200

    Added BlockEvent in TimeEvents.java

--- src/main/java/eu/baumistlustig/varo/events/TimeEvents.java
+++ src/main/java/eu/baumistlustig/varo/events/TimeEvents.java
@@ -5,16 +5,25 @@ import eu.baumistlustig.varo.utils.Timer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class TimeEvents implements Listener {
 
+    Timer timer = Varo.getPlugin().getTimer();
+
     // Peacetime
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onAttack(EntityDamageByEntityEvent e) {
         if (!(e.getEntity() instanceof Player)) return;
 
-        Timer timer = Varo.getPlugin().getTimer();
         if (timer.getTime() <= 300 && e.getDamager() instanceof Player) e.setCancelled(true);
+    }
+
+    // BlockEvent
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        Player player = e.getPlayer();
+        if (!timer.isRunning() || player.isOp() || player.hasPermission("*")) e.setCancelled(true);
     }
 }
